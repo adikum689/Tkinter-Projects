6b883868ac81e4175918af37df8e69f61f9a4691
adikum689 <161996271+adikum689@users.noreply.github.com>
2024-09-28 20:52:39 -0700

    Add files via upload

--- Calendar.py
+++ Calendar.py
@@ -1,14 +1,33 @@
-import tkinter
+import tkinter, calendar 
 screen=tkinter.Tk()
 screen.geometry("600x400") 
 screen.configure(background="white")
 screen.title("Calendar")
-label=tkinter.Label(screen, text= "Calendar", font= ("arial", 40))
+
+def show_calendar():
+    screen1= tkinter.Tk()
+    screen1.geometry("600x600")
+    year= int(entry.get())
+    screen1.configure(background= "grey")
+    screen1.title(year)
+    text= tkinter.Text(screen1, height= 50)
+    text.pack()
+    calendartext= calendar.calendar(year)
+    text.insert("1.0", calendartext)
+    screen1.mainloop()
+
+
+
+def exit():
+    screen.destroy()
+label=tkinter.Label(screen, text= "Calendar", font= ("arial", 50))
 label.pack()
-entry=tkinter.Entry(screen, text="Enter Year")
+label1=tkinter.Label(screen, text="Enter Year", font= ("arial", 30))
+label1.pack()
+entry=tkinter.Entry(screen)
 entry.pack()
-button= tkinter.Button(screen, text= "Show Calendar")
+button= tkinter.Button(screen, text= "Show Calendar", command= show_calendar)
 button.pack()
-exit_button= tkinter.Button (screen, text= "Exit")
+exit_button= tkinter.Button(screen, text= "Exit", command= exit)
 exit_button.pack()
 screen.mainloop()
